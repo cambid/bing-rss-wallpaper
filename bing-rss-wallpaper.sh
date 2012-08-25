e062d8cbb051ced6f538dbd385e3ba69a69d9fe9
Jan Fader <jan.fader@web.de>
2012-08-25 21:26:15 +0200

    removed dependancy for xmlcat

--- bing-rss-wallpaper.sh
+++ bing-rss-wallpaper.sh
@@ -4,15 +4,16 @@
 # To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send
 # a letter to Creative  Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 
-TEMPDIR=$(mktemp -d)
 if [ -z $1 ]; then
   echo "please specify an url"
+  exit 3
 fi
+TEMPDIR=$(mktemp -d)
 URL=$1
 wget -q $URL -O - | sed -e 's/$//' >>$TEMPDIR/theme
 RSSURL=$(grep RSSFeed $TEMPDIR/theme | cut -f 2- -d '=')
 wget -q $RSSURL -O - | sed -e 's/$//' >>$TEMPDIR/rss
-WALLPAPERIMG=$(./xmlcat $TEMPDIR/rss | grep "enclosure url" | cut -f 2 -d '"' | shuf -n1)
+WALLPAPERIMG=$(sed -e 's/\(<[^<>]\)/\n\1/g' $TEMPDIR/rss | grep "enclosure url" | cut -f 2 -d '"' | shuf -n1)
 wget -q "$WALLPAPERIMG" -O $TEMPDIR/img
 feh --bg-scale $TEMPDIR/img
 rm -r ${TEMPDIR}
